716e6cc6a6e34aff2f4e02cf1f52a2988857de7f
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2022-09-03 22:53:54 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector(".btn"),
+    tip = document.querySelector(".tip"),
+    total = document.querySelector(".total"),
+    err = document.querySelector(".error");
+
+
+    
+
+const calTip = () => {
+    const bill = document.querySelector(".bill").value;
+    const rt = document.querySelector(".rate").value;
+
+    if (bill === "" || rt === "") {
+        err.style.display = "block";
+        hiderr();
+    } else if(isNaN(bill)){
+        err.innerHTML = "Please, Enter Only Number";
+        err.style.display = "block";
+        hiderr();
+    }
+    else {
+        let tipAmt = bill * rt;
+        tip.innerHTML = `Tip $ ${tipAmt}`
+    }
+}
+btn.addEventListener("click", calTip);
+
+const hiderr = () => {
+  setTimeout(() => {
+    err.style.display = "none";
+  }, 6000);
+};
+
